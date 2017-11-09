9493a627e5d5beb534e753fd75642277b79eea7d
Diastuti Utami <13514071@std.stei.itb.ac.id>
2017-11-09 09:37:18 +0700

    initial commit

---
+++ simple_sentiment_analysis.py
@@ -0,0 +1,48 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import nltk
+from nltk.corpus import sentiwordnet as swn
+
+doc = "Trump is truly disgusting isnt he? Look at the troll, such tiny hands and so unpresidential he is truly a conman"
+
+stokens = [nltk.word_tokenize(sent) for sent in sentences]
+taggedlist=[]
+for stoken in stokens:
+     taggedlist.append(nltk.pos_tag(stoken))
+wnl = nltk.WordNetLemmatizer()
+
+score_list=[]
+for idx,taggedsent in enumerate(taggedlist):
+    score_list.append([])
+    for idx2,t in enumerate(taggedsent):
+        newtag=''
+        lemmatized=wnl.lemmatize(t[0])
+        if t[1].startswith('NN'):
+            newtag='n'
+        elif t[1].startswith('JJ'):
+            newtag='a'
+        elif t[1].startswith('V'):
+            newtag='v'
+        elif t[1].startswith('R'):
+            newtag='r'
+        else:
+            newtag=''
+        if(newtag!=''):
+            synsets = list(swn.senti_synsets(lemmatized, newtag))
+            #Getting average of all possible sentiments, as you requested
+            score=0
+            if(len(synsets)>0):
+                for syn in synsets:
+                    score+=syn.pos_score()-syn.neg_score()
+                score_list[idx].append(score/len(synsets))
+
+print(score_list)
+sentence_sentiment=[]
+
+for score_sent in score_list:
+    sentence_sentiment.append(sum([word_score for word_score in score_sent])/len(score_sent))
+print("Sentiment for each sentence for:"+doc)
+print(sentence_sentiment)
