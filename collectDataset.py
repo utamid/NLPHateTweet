0d412cb52c07c1ba39ff36a5c282ee57209fc65c
M. Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-11-15 13:26:17 +0700

    Get full 280 character tweets

--- collectDataset.py
+++ collectDataset.py
@@ -14,19 +14,20 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-queries = ['@awkarin', '@realDonaldTrump', '@pewdiepie','@BarackObama', '@MichelleObama', '@IvankaTrump', '@justinbieber']
+queries = ['@realDonaldTrump', '@pewdiepie','@BarackObama', '@MichelleObama', '@IvankaTrump', '@justinbieber']
 
 total_data = 0
 for query in queries:
     max_tweets = 250
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q=query, tweet_mode="extended").items(max_tweets)]
     with open('data.json', 'r') as fin:
         data = json.loads(fin.read())
 
+    pprint.pprint(dir(searched_tweets[0]))
     for tweet in searched_tweets:
-        if (tweet.lang == 'en' and 'RT' not in tweet.text):
+        if (tweet.lang == 'en' and 'RT' not in tweet.full_text):
             data.append({
-                'text': tweet.text,
+                'text': tweet.full_text,
                 'time': str(tweet.created_at),
                 'username': tweet.user.screen_name,
                 'full_name': tweet.user.name,
